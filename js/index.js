400844025c78947f450087f3290368d341c08205
eduardochvzb <eduardochvzb@gmail.com>
2020-12-05 19:09:16 -0600

    fixed transition and animation events

--- js/index.js
+++ js/index.js
@@ -4,41 +4,62 @@ import introHero from './introHero.js';
 introHero();
 
 const navBar = document.querySelector('nav');
-const btnControllers = document.querySelectorAll('.btn');
-const fadeTextClass = 'fade-text-';
-const changeElem = [
-    [document.querySelector('.img-container'), 'enter-img-container'],
-    [document.querySelector('img'), 'enter-img'],
-    [document.querySelector('.info'), 'enter-info'],
-    [document.querySelector('.title'), fadeTextClass + 'title'],
-    [document.querySelector('.description'), fadeTextClass + 'description'],
-    [document.querySelector('.date'), fadeTextClass + 'date']
-];
+const btnControllers = [document.querySelector('#back'), document.querySelector('#front')];
+const changeElem = document.querySelectorAll('.changeElem');
+const photoElem = document.querySelector('img');
 
-const photoElem = changeElem[1][0];
 let currentIndex = 'p0';
+let started = true;
 
-function toggleClassNames(arr) {
-    arr.forEach(subArr => {
-        subArr[0].classList.toggle(subArr[1]);
+function removeClassName(elem, className){
+    elem.classList.remove(className);
+}
+function addClassName(elem, className){
+    elem.classList.add(className);
+}
+
+function toggleClassName(elem, customClassName, remove) {
+    let className = customClassName || elem.dataset.cssanim;
+
+    if(remove){
+        removeClassName(elem, className);
+        return;
+    }
+    addClassName(elem, className);
+}
+
+function callClassNames(arr = changeElem) {
+    arr.forEach(elem => {
+        toggleClassName(elem);
     })
 }
-function callClassNames(arr = changeElem) {
-    toggleClassNames(arr);
-    setTimeout(() => {
-        toggleClassNames(arr);
-    }, 800);
+function toggleDisableBtnControllers(p) {
+    let pInt = +p.slice(1, p.length);
+    const toggleDisable = i => {
+        let btnIsDisable = btnControllers[i].classList.contains('disable');
+        toggleClassName(btnControllers[i], 'disable', btnIsDisable);
+    }
+    if (pInt === 0) {
+        toggleDisable(0);
+    } else if (pInt === data.length - 1) {
+        toggleDisable(1);
+    }
 }
-function setData(obj, indexes, arrowBtnArr) {
+function setData(obj, indexes, arrowBtn) {
+    photoElem.style.opacity = '0';
     indexes.forEach(p => {
         if (p === null) return;
         document.querySelector('#' + p).classList.toggle('active');
+        if(started){
+            started = !started;
+            return;
+        }
+        toggleDisableBtnControllers(p);
     });
     for (let key in obj) {
         document.querySelector('.' + key).innerText = obj[key];
     }
-    if(arrowBtnArr) callClassNames([arrowBtnArr]);
-    photoElem.style.transform = 'scale(0)'; 
+    if (arrowBtn) callClassNames([arrowBtn]);
     photoElem.setAttribute('src', `img/${indexes[0].slice(1, indexes[0].length)}.jpg`);
 }
 
@@ -51,7 +72,7 @@ function clickBtnAnim(btn) {
 
     btn.setAttribute('disable', 'true');
 
-    setData(data[tempI], ['p' + tempI, currentIndex], [btn, 'clicked']);
+    setData(data[tempI], ['p' + tempI, currentIndex], btn);
 
     currentIndex = 'p' + tempI;
     setTimeout(() => {
@@ -62,21 +83,17 @@ function clickButtonArrow(e) {
     clickBtnAnim(this);
 }
 
-btnControllers.forEach(btn => {
-    btn.addEventListener('click', clickButtonArrow);
-})
-
 photoElem.addEventListener('load', e => {
-    photoElem.style.transform = 'scale(1)'; 
+    photoElem.style.opacity = '1';
     callClassNames();
 })
 
 document.addEventListener('keydown', ({ code }) => {
     if (code == 'KeyA' || code == 'ArrowLeft') {
-        clickBtnAnim(document.querySelector('#back'));
+        clickBtnAnim(btnControllers[0]);
     }
     else if (code == 'KeyD' || code == 'ArrowRight') {
-        clickBtnAnim(document.querySelector('#front'));
+        clickBtnAnim(btnControllers[1]);
     }
 });
 
@@ -93,5 +110,19 @@ for (let i = 0; i < data.length; i++) {
 
     navBar.appendChild(navPoint);
 }
+changeElem.forEach(elem => {
+    elem.addEventListener('animationend', e => {
+        toggleClassName(e.target, '', true);
+    })
+})
+btnControllers.forEach(btn => {
+    btn.addEventListener('click', clickButtonArrow);
+    btn.addEventListener('transitionend', e => {
+        toggleClassName(e.target, '', true);
+    })
+});
+document.querySelector('.info').addEventListener('transitionend', e => {
+    toggleClassName(e.target, '', true);
+})
 
 setData(data[0], [currentIndex, null]);
